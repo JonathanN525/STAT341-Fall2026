3af0833247d259dfaa2641a838e7ed3b8b54eb11
Jonathan Noriega <jnoriega@claflin.edu>
2026-08-18 16:15:22 -0400

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,20 @@
+# Name: Jonathan Noriega, Date: 08/18/2026, Purpose: Test functions of descriptive statistics
+
+#Create a variable called variable1 and store 5 numbers from 1-5
+variable1 <- c(1:5)
+variable1
+[1] 1 2 3 4 5
+
+# Calculate mean of variable1
+mean(variable1)
+[1] 3
+
+# Calculate standard deviation
+sd(variable1)
+[1] 1.581139
+
+
+# Find median
+median(variable1)
+[1] 3
+
